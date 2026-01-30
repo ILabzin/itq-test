e62af938cbd0a22203ef8758d40d8ffbe903800e
Ivan Labzin <i.labzin@mail.astondevs.ru>
2026-01-30 12:33:44 +0300

    init commit

---
+++ src/main/java/com/labzin/document_service_itq_test_api/DocumentServiceItqTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.labzin.document_service_itq_test_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentServiceItqTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentServiceItqTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labzin/document_service_itq_test_api/DocumentServiceItqTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labzin.document_service_itq_test_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentServiceItqTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
